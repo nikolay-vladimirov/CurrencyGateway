7621b65c6f130e68d96fd9e169d19ed67d2988e9
nikolay-vladimirov <nik.ev.vlad@gmail.com>
2024-09-28 16:38:46 +0300

    initial commit

---
+++ src/main/java/vladimirov/nikolay/CurrencyGateway/CurrencyGatewayApplication.java
@@ -0,0 +1,13 @@
+package vladimirov.nikolay.CurrencyGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vladimirov/nikolay/CurrencyGateway/CurrencyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package vladimirov.nikolay.CurrencyGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
